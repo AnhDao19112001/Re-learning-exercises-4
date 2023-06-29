a22d431e5dd27b8a8ce1517e60a0e825d6adf866
AnhDao19112001 <phantaanhdao@gmail.com>
2023-06-29 19:17:44 +0700

    update bai tap

--- bai_10/src/main/java/com/example/bai_10/Bai10Application.java
+++ bai_10/src/main/java/com/example/bai_10/Bai10Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Bai10Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)   {
         SpringApplication.run(Bai10Application.class, args);
     }
 
