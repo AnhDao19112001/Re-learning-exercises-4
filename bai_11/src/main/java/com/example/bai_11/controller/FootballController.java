26aa22eced0e56cccf914f7eccae6329a27b6f04
AnhDao19112001 <phantaanhdao@gmail.com>
2023-07-04 10:13:19 +0700

    update bai tap 12

--- bai_11/src/main/java/com/example/bai_11/controller/FootballController.java
+++ bai_11/src/main/java/com/example/bai_11/controller/FootballController.java
@@ -24,17 +24,17 @@ public class FootballController {
         }
         return new ResponseEntity<>(footballList, HttpStatus.OK);
     }
-    @GetMapping("/view/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<Football> showDetailFootball(@PathVariable int id){
         Optional<Football> football = iFootballService.findById(id);
 
         return football.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
-    @PostMapping("/create")
+    @PostMapping()
     public ResponseEntity<Football> createFootball(@RequestBody Football football){
         return new ResponseEntity<>(iFootballService.create(football),HttpStatus.CREATED);
     }
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Football> deleteFootball(@PathVariable Integer id){
         Optional<Football> football = iFootballService.findById(id);
         if (!football.isPresent()){
@@ -43,7 +43,7 @@ public class FootballController {
         iFootballService.delete(id);
         return new ResponseEntity<>(football.get(), HttpStatus.NO_CONTENT);
     }
-    @PutMapping("/update/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Football> updateFootball(@PathVariable Integer id, @RequestBody Football football){
         Optional<Football> football1 = iFootballService.findById(id);
         if (!football1.isPresent()){
